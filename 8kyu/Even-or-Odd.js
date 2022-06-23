a6b4e138716c31df8f630506657c2613a4dc9f2e
JeffreyEyed <93097681+JeffreyEyed@users.noreply.github.com>
2022-06-22 20:50:44 -0400

    multiply

---
+++ 8kyu/Even-or-Odd.js
@@ -0,0 +1,18 @@
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+
+//My solution
+//#1
+function even_or_odd(number) {
+    if (number%2 == 0) {
+      return "Even";
+    } else {
+      return "Odd";
+    }
+  }
+
+
+  //#2
+  function even_or_odd(number) {
+    return number % 2 ? "Odd" : "Even"
+  }
---
+++ 8kyu/multiply.js
@@ -0,0 +1,13 @@
+//This code does not execute properly. Try to figure out why.
+
+//Code Question
+function multiply(a, b){
+    a * b
+  }
+  
+
+  //Answer
+  function multiply(a, b){
+    return a * b
+  }
+  
