6985e9aff91c9c585dbbe436781b7808c9722c7c
JeffreyEyed <93097681+JeffreyEyed@users.noreply.github.com>
2022-09-19 21:47:37 -0400

    sum of all aurguments

---
+++ 8kyu/sum-of-all-aurguments.js
@@ -0,0 +1,22 @@
+// Write a function that finds the sum of all its arguments.
+
+// eg:
+
+// sum(1, 2, 3) // => 6
+// sum(8, 2) // => 10
+// sum(1, 2, 3, 4, 5) // => 15
+// TIPS:
+// ruby/python : http://lmgtfy.com/?q=Ruby+splat+operator
+
+// JS/Coffeescript : http://lmgtfy.com/?q=Javascript+arguments+variable
+
+// C: https://www.geeksforgeeks.org/variadic-functions-in-c/
+
+
+
+
+function sum() {
+    return Array.prototype.reduce.call(arguments, function(a, b) {
+        return a + b;
+    }, 0);
+}
